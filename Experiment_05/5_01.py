ca2bce09c243de3b2385a7b57a449b9cdf56db8e
Chetanmalage <147618750+Chetanmalage@users.noreply.github.com>
2026-01-05 16:47:07 +0000

    updated

--- Experiment_05/5_01.py
+++ Experiment_05/5_01.py
@@ -1,14 +1,14 @@
 # Selection Sort using Static Data
 
 def selection_sort(arr):
- n = len(arr)
- for i in range(n - 1):
-  min_index = i
-  for j in range(i + 1, n):
-   if arr[j] < arr[min_index]:
-    min_index = j
-  arr[i], arr[min_index] = arr[min_index], arr[i]
-
+    n = len(arr)
+    for i in range(n - 1):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        
 # Static array
 arr = [64, 25, 12, 22, 11]
 print("Before Sorting:", arr)
--- Experiment_05/5_02.py
+++ Experiment_05/5_02.py
@@ -1,20 +1,20 @@
 # Selection Sort using Dynamic Data
 
 def selection_sort(arr):
- n = len(arr)
- for i in range(n - 1):
-  min_index = i
-  for j in range(i + 1, n):
-   if arr[j] < arr[min_index]:
-    min_index = j
-  arr[i], arr[min_index] = arr[min_index], arr[i]
+    n = len(arr)
+    for i in range(n - 1):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
 
 # Dynamic input
 n = int(input("Enter number of elements: "))
 arr = []
 
 for i in range(n):
- arr.append(int(input(f"Enter element {i+1}: ")))
+    arr.append(int(input(f"Enter element {i+1}: ")))
 
 print("Before Sorting:", arr)
 
