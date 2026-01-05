b36c507dcde57a9d9ea41be808782f4af377bfea
Chetanmalage <147618750+Chetanmalage@users.noreply.github.com>
2026-01-05 16:43:10 +0000

    updated

--- Experiment_04/4_01.py
+++ Experiment_04/4_01.py
@@ -1,17 +1,17 @@
 # Binary Search using Static Data
 
 def binary_search(arr, key):
- low = 0
- high = len(arr) - 1
- while low <= high:
-  mid = (low + high) // 2
-  if arr[mid] == key:
-   return mid
-  elif key < arr[mid]:
-   high = mid - 1
-  else:
-   low = mid + 1
- return -1
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == key:
+            return mid
+        elif key < arr[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
   
 # Static sorted array
 arr = [5, 12, 18, 25, 36, 48, 59]
@@ -26,4 +26,4 @@ result = binary_search(arr, key)
 if result != -1:
     print("Element found at position:", result + 1)
 else:
- print("Element not found")
+    print("Element not found")
